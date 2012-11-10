87dcd74ec317e132a193ce80bf2e220e6c6d442d
Timothy Lin <lzh9102@gmail.com>
2012-11-10 13:00:02 +0800

    Add copyright notice.

--- queuedisplay.h
+++ queuedisplay.h
@@ -1,3 +1,14 @@
+/* qtypist - a typing training program
+ *
+ * Copyright (C) 2012 Timothy Lin <lzh9102@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 or 3 of the License.
+ *
+ * project page: http://code.google.com/p/qtypist
+ */
+
 #ifndef QUEUEDISPLAY_H
 #define QUEUEDISPLAY_H
 
